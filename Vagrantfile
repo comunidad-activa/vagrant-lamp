b128b417552a6c6ca3765f5542ff1d8fd9e28e06
Nicolas Cuevas <nicolasgcuevas@gmail.com>
2021-04-18 18:39:49 -0300

    updatin files

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,3 @@
-VAGRANTFILE_API_VERSION = "2"
-
 # UTC        	for Universal Coordinated Time
 # EST        	for Eastern Standard Time
 # US/Central 	for American Central
@@ -8,20 +6,41 @@ VAGRANTFILE_API_VERSION = "2"
 
 server_timezone  = "UTC"
 
-Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-    config.vm.box = "ubuntu/trusty64"
+Vagrant.configure("2") do |config|
+    config.vm.box = "ubuntu/bionic64"
 
-    #Установите локальный IP адрес и пропишите в файле hosts (пример: 192.168.33.22 my-project.app) доменное имя для доступа по доменному имени
-    #config.vm.network "private_network", ip: "192.168.33.22"
+    # etc/hosts config: 192.168.33.22 my-project.app
+    config.vm.network "private_network", ip: "192.168.50.23"
 
-    config.vm.network "forwarded_port", guest: 80, host: 8080
+    config.vm.network "forwarded_port", guest: 80, host: 9001
     config.vm.network "forwarded_port", guest: 3306, host: 33060
+
+    config.ssh.username = "ubuntu"
+    config.ssh.password = "vagrant"
+    config.ssh.insert_key = true
 
     config.vm.provider :virtualbox do |vb|
         # Use VBoxManage to customize the VM. For example to change memory:
-        vb.customize ["modifyvm", :id, "--memory", "384"]
+        vb.name = "fenix_vm"
+        vb.cpus = 1
+        vb.customize ["modifyvm", :id, "--memory", "512"]
     end
     # Set share folder permissions to 777 so that apache can write files.
     config.vm.synced_folder "./public", "/home/vagrant/public", :mount_options => ["dmode=777", "fmode=666"]
     config.vm.provision :shell, :path => "bootstrap.sh", :args => [server_timezone]
+    config.vm.post_up_message = <<-MESSAGE
+        
+          .\\            //.
+        . \ \          / /.
+        .\  ,\     /` /,.-
+         -.   \  /'/ /  .
+         ` -   `-'  \  -
+           '.       /.\`
+              -    .-
+              :`//.'
+              .`.'
+              .'  FENIX VM 1.0
+                  2021
+
+    MESSAGE
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 # Variables path
 php_config_file="/etc/php5/apache2/php.ini"
-xdebug_config_file="/etc/php5/mods-available/xdebug.ini"
+#xdebug_config_file="/etc/php5/mods-available/xdebug.ini"
 mysql_config_file="/etc/mysql/my.cnf"
 apache_config_file="/etc/apache2/apache2.conf"
 host_document_root="/home/vagrant"
@@ -15,21 +15,27 @@ DBNAME=vagrant
 DBUSER=vagrant
 DBPASSWD=vagrant
 
-echo "--- Начало установки ---"
+echo "--- Setup Start ---"
 
-echo "--- Обновление списка пакетов ---"
+echo "--- update packages ---"
 apt-get -qq update
 
-echo "--- Установка базовых пакетов ---"
-apt-get -y install vim curl build-essential python-software-properties git
+echo "--- install common packages ---"
+apt-get -y install vim curl build-essential python-software-properties git curl htop python-pip
 
-echo "--- Обновление списка пакетов ---"
+echo "--- update command after install common packages ---"
 apt-get update
 
-echo "--- Установка Apache2 ---"
+echo "--- Install Apache2 ---"
 apt-get install -y apache2
 
-echo "--- Установка MySql ---"
+echo "--- Install MongoDB ---"
+apt-get install -y mongodb
+
+echo "--- Install RedisDB ---"
+apt-get install -y redis-server
+
+echo "--- Install MySql ---"
 echo "mysql-server mysql-server/root_password password ${DBPASSWD}" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password ${DBPASSWD}" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
@@ -37,81 +43,55 @@ echo "phpmyadmin phpmyadmin/app-password-confirm password ${DBPASSWD}" | debconf
 echo "phpmyadmin phpmyadmin/mysql/admin-pass password ${DBPASSWD}" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/mysql/app-pass password ${DBPASSWD}" | debconf-set-selections
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
-apt-get -y install mysql-server-5.5 mysql-client phpmyadmin
+apt-get -y install mysql-server mysql-client phpmyadmin
 
 mysql -uroot -p${DBPASSWD} -e "CREATE DATABASE ${DBNAME}"
 mysql -uroot -p${DBPASSWD} -e "grant all privileges on $DBNAME.* to '${DBUSER}'@'localhost' identified by '${DBPASSWD}'"
 
-echo "--- Установка репозиториев ---"
-add-apt-repository ppa:ondrej/php5-5.6
-
-#uncomment to install NodeJS
-#add-apt-repository ppa:chris-lea/node.js
+echo "--- add PHP repo ---"
+add-apt-repository ppa:ondrej/php
 
 apt-get -qq update
 
-echo "--- Установка PHP ---"
-apt-get install -y php5 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-mysql php-apc
-php5enmod mcrypt
+echo "--- Install PHP ---"
+apt-get install -y software-properties-common php7.4 libapache2-mod-php7.4 php7.4-curl php7.4-gd php7.4-mcrypt php7.4-mysql php7.4-common php7.4-curl
 
-echo "--- Установка и конфигурация xDebug ---"
-apt-get install -y php5-xdebug
+#echo "--- Install xDebug ---"
+#apt-get install -y php5-xdebug
+# cat << EOF | sudo tee -a ${xdebug_config_file}
+# xdebug.scream=1
+# xdebug.cli_color=1
+# xdebug.show_local_vars=1
+# EOF
 
-cat << EOF | sudo tee -a ${xdebug_config_file}
-xdebug.scream=1
-xdebug.cli_color=1
-xdebug.show_local_vars=1
-EOF
-
-echo "--- Включение mod-rewrite ---"
+echo "--- update apache config ---"
 a2enmod rewrite
 
-echo "--- Установка root папки---"
 sudo rm -rf ${apache_document_root}
 sudo ln -fs ${host_document_root}/${project_folder_name} ${apache_document_root}
 
-echo "--- Настройка php.ini и apache2.conf ---"
+echo "--- update php.ini and apache2.conf ---"
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" ${php_config_file}
 sed -i "s/display_errors = .*/display_errors = On/" ${php_config_file}
 
-#uncomment to work with Boris
-#echo "--- Работа с Boris---"
-#sed -i "s/disable_functions = .*//" /etc/php5/cli/php.ini
 
 sudo sed -i 's/AllowOverride None/AllowOverride All/g' ${apache_config_file}
 
 a2enconf phpmyadmin
 
-echo "--- Перезагрузка Apache2 ---"
+echo "--- restart Apache2 ---"
 service apache2 restart
 
-echo "--- Перезагрузка mysql ---"
+echo "--- restart mysql ---"
 service mysql restart
 
-#echo "--- Установка composer ---"
-#uncomment to install coomposer
-#curl --silent https://getcomposer.org/installer | php
-#mv composer.phar /usr/local/bin/composer
+echo "--- install composer ---"
+curl --silent https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
 
-#echo "--- Установка NodeJS и NPM ---"
-#uncomment to install NodeJS NPM
-#curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
-#apt-get -y install nodejs
+echo "--- install lts nodejs NodeJS y NPM ---"
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+apt-get -y install nodejs
 
-#echo "--- Установка Gulp, Bower ---"
-#uncomment to install Gulp and Bower
-#npm install -g gulp bower
-
-#echo "--- Обновление компонентов проекта ---"
-#uncomment to update packages
-#cd /vagrant
-#sudo -u vagrant -H sh -c "composer install"
-#cd /vagrant/client
-#sudo -u vagrant -H sh -c "npm install"
-#sudo -u vagrant -H sh -c "bower install -s"
-#sudo -u vagrant -H sh -c "gulp"
-
-#uncomment to setting timezone
-#echo "Setting Timezone to $1"
-#sudo ln -sf /usr/share/zoneinfo/$1 /etc/localtime
-
+echo "--- install node common packages ---"
+npm install -g yarn pm2
